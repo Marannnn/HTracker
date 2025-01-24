08cd48968940c760865be45c7f1922b5cbab51d9
Marannnn <martin.hrubes1@seznam.cz>
2025-01-24 20:48:42 +0100

    Some changes
    Add content, Reset all, Days remaining updates when daysCounter changes(works with OnDaysCounterChanged)

--- HTracker/HTracker/ViewModels/MainViewModel.cs
+++ HTracker/HTracker/ViewModels/MainViewModel.cs
@@ -2,31 +2,56 @@
 using CommunityToolkit.Mvvm.Input;
 using HTracker.Model;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 
 namespace HTracker.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    ObservableCollection<Habit> HabitList { get; } = new ObservableCollection<Habit>(); //kolekce, aby se mohl itemsControl aktualizovat kdykoliv se neco prida nebo odebere
+    ObservableCollection<Habit> HabitCollection { get; } = new ObservableCollection<Habit>(); //kolekce, aby se mohl itemsControl aktualizovat kdykoliv se neco prida nebo odebere
 
     [ObservableProperty]
-    private ushort _currentDay;
+    private uint _currentDay;
     [ObservableProperty]
-    private ushort _daysCount;
+    private uint _daysRemaining;
     [ObservableProperty]
-    private ushort _daysRemaining;
+    private uint _daysCount;
 
     [ObservableProperty]
     private string _content;
 
+    partial void OnDaysCountChanged(uint value) //tahle metoda je tvorena automaticky diky [ObservableProperty] a spusti se kdykoliv, kdyz se _daysCount zmeni. Ja tady tu metodu jenom "specifikuju" a urcuju ji logiku, co se stane, sama o sobe uz ale existuje
+    {
+        if (DaysCount > CurrentDay)
+        {
+            DaysRemaining = DaysCount - CurrentDay;
+        }
+    }
+
     [RelayCommand]
     public void AddHabit()
     {
-        Habit habit = new Habit()
+        if (string.IsNullOrWhiteSpace(Content) == false)
         {
-            content = _content
-        };
-        HabitList.Add(habit); //pridam objekt do kolekce
+            Habit habit = new Habit()
+            {
+                content = Content 
+            };
+            HabitCollection.Add(habit); //pridam objekt do kolekce
+            Content = string.Empty;
+            Debug.WriteLine(habit.content);
+        }
     }
-
+    [RelayCommand]
+    public void RemoveAll()
+    {
+        foreach (var habit in HabitCollection)
+        {
+            HabitCollection.Remove(habit);
+        }
+        Content = string.Empty;
+        CurrentDay = 0u;
+        DaysCount = 0u;
+        DaysRemaining = 0u;
+    }
 }
