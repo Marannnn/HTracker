eaa99ddfe6869db47e110c3d1df5d6ae81357896
Marannnn <martin.hrubes1@seznam.cz>
2025-01-26 17:32:55 +0100

    List all habits
    ItemsControl now shows all of added habits

--- HTracker/HTracker/ViewModels/MainViewModel.cs
+++ HTracker/HTracker/ViewModels/MainViewModel.cs
@@ -9,6 +9,7 @@ namespace HTracker.ViewModels;
 public partial class MainViewModel : ViewModelBase
 {
     ObservableCollection<Habit> HabitCollection { get; } = new ObservableCollection<Habit>(); //kolekce, aby se mohl itemsControl aktualizovat kdykoliv se neco prida nebo odebere
+    ObservableCollection<Habit> CompletedHabitCollection { get; } = new();
 
     [ObservableProperty]
     private uint _currentDay;
@@ -18,8 +19,7 @@ public partial class MainViewModel : ViewModelBase
     private uint _daysCount;
 
     [ObservableProperty]
-    private string _content;
-
+    private string _text;
     partial void OnDaysCountChanged(uint value) //tahle metoda je tvorena automaticky diky [ObservableProperty] a spusti se kdykoliv, kdyz se _daysCount zmeni. Ja tady tu metodu jenom "specifikuju" a urcuju ji logiku, co se stane, sama o sobe uz ale existuje
     {
         if (DaysCount > CurrentDay)
@@ -31,27 +31,27 @@ public partial class MainViewModel : ViewModelBase
     [RelayCommand]
     public void AddHabit()
     {
-        if (string.IsNullOrWhiteSpace(Content) == false)
+        if (string.IsNullOrWhiteSpace(Text) == false) //jestli uzivatel neco napsal
         {
-            Habit habit = new Habit()
+            HabitCollection.Add(new Habit() //pridam novy element do kolekce, objekt Habit a pridam mu vlastnosti. XAML content pak odkazuje na "content" tohoto objektu
             {
-                content = Content 
-            };
-            HabitCollection.Add(habit); //pridam objekt do kolekce
-            Content = string.Empty;
-            Debug.WriteLine(habit.content);
+                content = Text,
+                isCompleted = false,
+            });
+            Text = string.Empty;
         }
     }
-    [RelayCommand]
-    public void RemoveAll()
+    public void ResetAll()
     {
-        foreach (var habit in HabitCollection)
-        {
-            HabitCollection.Remove(habit);
-        }
-        Content = string.Empty;
-        CurrentDay = 0u;
-        DaysCount = 0u;
-        DaysRemaining = 0u;
+        DaysCount = 0;
+        DaysRemaining = 0;
+        CurrentDay = 0;
+        HabitCollection.Clear();
+    }
+
+    [RelayCommand]
+    public void CheckedHabit()
+    {
+
     }
 }
